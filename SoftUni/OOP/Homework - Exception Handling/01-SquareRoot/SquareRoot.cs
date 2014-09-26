5a9dacb72e7a76854b1bed4a18f38c899b2b3f96
Krassimir <xhism.kml@gmail.com>
2014-09-26 21:00:29 +0300

    OOP Exception Handling Homework

---
+++ SoftUni/OOP/Homework - Exception Handling/01-SquareRoot/SquareRoot.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_SquareRoot
+{
+    class SquareRoot
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            try
+            {
+                int num = int.Parse(input);
+                if (num < 0)
+                {
+                    throw new ArithmeticException();
+                }
+                double sqrt = Math.Sqrt(num);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            catch
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good Bye");
+            }
+        }
+    }
+}
